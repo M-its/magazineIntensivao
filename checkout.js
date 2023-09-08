f39a97072f286adc6ade1deebb9fb2701c8d3215
Mitsrael Souza <mitarael9@gmail.com>
2023-09-08 16:05:59 -0300

    first commit

---
+++ checkout.js
@@ -0,0 +1,45 @@
+import {
+    desenharProdutoCarrinhoSimples,
+    lerLocalStorage,
+    apagarDoLocalStorage,
+    savarLocalStorage,
+} from "./src/utilidades"
+
+import { atualizarPrecoCarrinho } from "./src/menuCarrinho"
+
+function desenharProdutosCheckout() {
+    const idsProdutoCarrinhoComQuantidade = lerLocalStorage("carrinho") ?? {}
+    for (const idProduto in idsProdutoCarrinhoComQuantidade) {
+        desenharProdutoCarrinhoSimples(
+            idProduto,
+            "container-produtos-checkout",
+            idsProdutoCarrinhoComQuantidade[idProduto]
+        )
+    }
+}
+
+function finalizarCompra(evento) {
+    evento.preventDefault()
+    const idsProdutoCarrinhoComQuantidade = lerLocalStorage("carrinho") ?? {}
+    if (Object.keys(idsProdutoCarrinhoComQuantidade).length === 0) {
+        return
+    }
+
+    const dataAtual = new Date()
+    const pedidoFeito = {
+        dataPedido: dataAtual,
+        pedido: idsProdutoCarrinhoComQuantidade,
+    }
+    const historicoDePedidos = lerLocalStorage("historico") ?? []
+    const historicoDePedidosAtualizado = [pedidoFeito, ...historicoDePedidos]
+
+    savarLocalStorage("historico", historicoDePedidosAtualizado)
+    apagarDoLocalStorage("carrinho")
+
+    window.location.href = "./pedidos.html"
+}
+
+atualizarPrecoCarrinho()
+desenharProdutosCheckout()
+
+document.addEventListener("submit", (evt) => finalizarCompra(evt))
